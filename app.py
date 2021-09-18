90b30d1697bd78810a50b4494cc69868ac7b743b
alexandrosanat <alexandrosanat@gmail.com>
2021-09-18 22:56:32 +0100

    Trained model and developed API

--- app.py
+++ app.py
@@ -1,17 +1,39 @@
 import uvicorn
 from fastapi import FastAPI
+from data_model import BankNote
+import numpy as np
+import pandas as pd
+import pickle
 
 app = FastAPI()
+
+with open('model.pkl', 'rb') as file:
+    model = pickle.load(file)
 
 
 @app.get('/')
 def index():
-    return {'message': 'Hello world!'}
+    return {'message': 'Model serving API'}
 
 
-@app.get('/welcome')
-def get_name(name: str):
-    return {'Welcome to this page {}'.format(name)}
+@app.post('/predict')
+def predict_banknote(data: BankNote):
+    variance = data.variance
+    skewness = data.skewness
+    curtosis = data.curtosis
+    entropy = data.entropy
+
+    prediction = model.predict([[variance,
+                                skewness,
+                                curtosis,
+                                entropy]])
+
+    if prediction[0] > 0.5:
+        result = 'The Note is fake.'
+    else:
+        result = 'The Note is real.'
+
+    return {'prediction': result}
 
 
 if __name__ == '__main__':
